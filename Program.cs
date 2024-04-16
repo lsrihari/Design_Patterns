6c42897898a8bba874b7c948b959b88b327f1c5b
Srihari Lakshminarasimhan <v-sriharil@microsoft.com>
2024-04-16 10:25:46 +0530

    Comment added

--- Program.cs
+++ Program.cs
@@ -26,6 +26,7 @@ namespace DesignPatterns
             #endregion
 
             #region Strategy
+            //To use when all obects varies between several behaviour dynammically
             Animal sparky = new Dog();
             Animal kitty = new Cat(); 
             Animal tweety = new Bird();
